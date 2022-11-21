eaa7c276c2b0b1d1361a69f1c7cad49c5b03be72
Rye Terrell <ryeterrell@ryeterrell.net>
2022-11-20 19:25:22 -0600

    no core impacts

--- src/controller/game.ts
+++ src/controller/game.ts
@@ -397,7 +397,7 @@ export async function game(resources: Resources, playerDrone: PlayerDrone) {
 
           // Handle impact.
           const IMPACT_SCALE = 1;
-          if (state.player.impact && target != state.player) {
+          if (state.player.impact && target != state.player && !target.isCore) {
             target.rotation += IMPACT_SCALE * Math.random() - 0.5 * IMPACT_SCALE;
           }
 
