e0a6a834d52970e477fac13d06b5f4105939d3c6
Rye Terrell <ryeterrell@ryeterrell.net>
2022-11-24 17:19:01 -0600

    version management :|

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { loadingScreen } from "./controller/loading";
 import { mainMenu } from "./controller/main-menu";
 import { setMenuResources } from "./controller/menu";
 
+const version = localStorage.getItem("version");
+if (version === null) {
+  localStorage.clear();
+  localStorage.setItem("version", "1");
+  location.reload();
+}
+
 async function main() {
   await RAPIER.init();
   const resources = await loadingScreen();
