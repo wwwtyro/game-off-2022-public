7f74e2a5e76d4de8c6ec33dd7daf3916ba2c5bf0
Rye Terrell <ryeterrell@ryeterrell.net>
2022-11-28 19:59:28 -0600

    tweak tips

--- src/controller/tip.ts
+++ src/controller/tip.ts
@@ -9,7 +9,7 @@ const tips = [
   "Armor upgrades provide more defense than shield upgrades, but armor must be repaired by spending an upgrade while shields recharge automatically.",
   "It's a good idea to balance your upgrades. If you have ten power upgrades and one firing rate upgrade, taking another power upgrade will increase your DPS by 10%, but taking another firing rate upgrade will increase it by 100%.",
   "Many upgrades have limits. If you reach those limits, they will not consume space on your upgrade lottery.",
-  "Missiles are ten times more powerful than beams and fire 1/10th as frequently for the same number of upgrades, but they do not need to be aimed and they have 50% more targeting range.",
+  "Missiles are ten times more powerful than beams and fire 1/10th as frequently for the same number of upgrades, but they do not need to be aimed and they have a 50% greater targeting range.",
   "There are limits to the number of times some upgrades can be taken, but those can be exceeded with the proper special drone unlocked. For example, the Fleet Drone can have a total of 48 droids, but 32 is the normal limit.",
   "The most common way to die is by approaching an enemy core carelessly and head-on.",
   "Droid firing range is the same as yours, but since they flit about in a cloud around you at some distance, they can extend your range.",
@@ -34,7 +34,9 @@ export async function tip() {
     menu.exit();
   }
   menu.addItem(
-    new MenuHTML(`<div style="text-align: center; font-style: italic"><span style="color: yellow">Tip</span>: ${tip}</div>`)
+    new MenuHTML(
+      `<div style="text-align: center; font-style: italic; margin-bottom: 32px"><span style="color: yellow;">Tip</span>: ${tip}</div>`
+    )
   );
   menu.addItem(
     new MenuButton("Continue", () => {
